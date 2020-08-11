92171fc875ed6ef46aa1fd5100235570456e0741
Estêvão Samuel Procópio Amaral <tevaum@gmail.com>
2020-08-11 00:50:25 -0300

    Adding react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,7 +13,19 @@ function App() {
         <p>Countries</p>
       </header>
 
-      <CountryList />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <CountryList />
+          </Route>
+          <Router path="/country/:id">
+            <p>Country details</p>
+          </Router>
+          <Route path="*">
+            <p>Country not found!</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
