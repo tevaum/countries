960144f43a020d5e294c219c5fa0a3a683f1f2ab
Estêvão Samuel Procópio Amaral <tevaum@gmail.com>
2020-08-13 13:13:29 -0300

    Finalizando 100% dos testes. App.test.js implementa a mesma renderizacao que index.js, porem usando Mocks

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+
+import { COUNTRY_LIST} from "./countrylist/CountryList";
 import App from './App';
 
-/*
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-*/
 test('nothing', () => {
-  expect(true);
+  const mocks = [{
+    request: {
+      query: COUNTRY_LIST
+    },
+    error: new Error('Oops!')
+  }];
+
+  const doc = render(
+    <MockedProvider mocks={mocks} addTypename={false} >
+      <App />
+    </MockedProvider>
+  );
+  doc.debug();
+  expect(doc.getByText('Countries')).toBeInTheDocument();
+  expect(doc.getByText('Loading...')).toBeInTheDocument();
+
+  act(() => doc.findByText('Error!'));
 })
+
