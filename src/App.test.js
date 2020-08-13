64e46445d4607daa90e11df87099efc7aef8c3c6
Estêvão Samuel Procópio Amaral <tevaum@gmail.com>
2020-08-13 13:54:50 -0300

    Removendo console.log e ezcluindo index.js do calculo de coverage

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import { MockedProvider } from '@apollo/client/testing';
 import { COUNTRY_LIST} from "./countrylist/CountryList";
 import App from './App';
 
-test('nothing', () => {
+test('nothing', async () => {
   const mocks = [{
     request: {
       query: COUNTRY_LIST
@@ -18,10 +18,10 @@ test('nothing', () => {
       <App />
     </MockedProvider>
   );
-  doc.debug();
+
   expect(doc.getByText('Countries')).toBeInTheDocument();
   expect(doc.getByText('Loading...')).toBeInTheDocument();
 
-  act(() => doc.findByText('Error!'));
+  await act(() => doc.findByText('Error!'));
 })
 
